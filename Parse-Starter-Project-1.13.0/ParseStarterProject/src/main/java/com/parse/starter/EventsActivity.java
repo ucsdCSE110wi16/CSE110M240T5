062923451f3d0eb138a74629ee927b18fb4f8c09
Jenny Wong <jew007@ucsd.edu>
2016-03-03 21:45:29 -0800

    Swipe to refresh finished

--- Parse-Starter-Project-1.13.0/ParseStarterProject/src/main/java/com/parse/starter/EventsActivity.java
+++ Parse-Starter-Project-1.13.0/ParseStarterProject/src/main/java/com/parse/starter/EventsActivity.java
@@ -1,16 +1,12 @@
 package com.parse.starter;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 
 import com.parse.ParseException;
 
@@ -25,60 +21,63 @@ import java.util.List;
 public class EventsActivity extends AppCompatActivity {
 
     private SwipeRefreshLayout swipeContainer;
-    public static final int eventsPerQuery = 10;
+    private RecyclerView rvEvents;
+    private List<Event> vents;
+    private EventsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
         setContentView(R.layout.activity_events);
 
-        // Lookup the recyclerview in activity layout
-        RecyclerView rvEvents = (RecyclerView) findViewById(R.id.rvEvents);
-
-        // Create adapter passing in the sample user data
-        List<Event> vents = null;
+        // Set up and load the recycler view
+        rvEvents = (RecyclerView) findViewById(R.id.rvEvents);
+        vents = null;
         try {
-            vents = EventsBundler.recentEvents(eventsPerQuery);
+            vents = EventsBundler.recentEvents(100);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        Context context = getApplicationContext();
-        EventsAdapter adapter = new EventsAdapter(vents);
-
-        // Attach the adapter to the recyclerview to populate items
+        adapter = new EventsAdapter(vents);
         rvEvents.setAdapter(adapter);
-
-        // Set layout manager to position the items
         rvEvents.setLayoutManager(new LinearLayoutManager(this));
 
-        // Lookup the swipe container view
+        // Set up the swipe container view
         swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
         // Setup refresh listener which triggers new data loading
         swipeContainer.setOnRefreshListener(new OnRefreshListener() {
             @Override
             public void onRefresh() {
-                onStart();
+                adapter.clear();
+                try {
+                    vents = EventsBundler.recentEvents(100);
+                    vents.add(0, new Event()); // for testing purposes since AddNewActivity not working.
+                    // once it's working, must test that new events are put on the top
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                adapter.addAll(vents);
+                swipeContainer.setRefreshing(false);
             }
         });
+
         // Configure the refreshing colors
         swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
                 android.R.color.holo_green_light,
                 android.R.color.holo_orange_light,
                 android.R.color.holo_red_light);
-
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
     }
-
-
 
 }
